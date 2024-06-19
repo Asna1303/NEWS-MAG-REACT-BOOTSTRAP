2b01a3fb387503a605a6f4aa8d7a50d0e5270462
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:46:55 +0530

    fetched the url

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -5,7 +5,10 @@ const Newsboard = () => {
     const [articles,setArticles]=useState([]);
     useEffect(()=>{
         let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=d2ae75dc41e34ebfa4bde1b6d7895927=${import.meta.env.API_KEY}`;
-    })
+    fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
+    
+},[])
+
   return (
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
