ef1322910c30f2cdd3bbf2d5859abb9086f53424
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:32:43 +0530

    increased the font size of logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><span className="badge">Newsmag</span></a>
+      <a class="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">Newsmag</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
