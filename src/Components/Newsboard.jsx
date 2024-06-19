8e928078b0d987868b48b1123b3174e7d3ae3e8c
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:36:58 +0530

    created a usestate variable

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 
 
 const Newsboard = () => {
+    const [articles,setArticles]=useState()
   return (
     <div>
-      <h2 className="text-center">Latest <span className="">News</span></h2>
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
     </div>
   )
 }
