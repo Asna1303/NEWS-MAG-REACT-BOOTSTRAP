01df08584f8e1dbb7a7ce1bdeec714626219042d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:23:38 +0530

    added newsboard component to app.js and imported it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./Components/Navbar"
+import Newsboard from "./Components/Newsboard"
 
 
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div>
      <Navbar/>
+     <Newsboard/>
     </div>
   )
 }
