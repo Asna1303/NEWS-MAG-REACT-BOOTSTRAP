61fe273814535f9df85fbb1470dbfc55abe2566a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:41:35 +0530

    added the api key

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 const Newsboard = () => {
     const [articles,setArticles]=useState([]);
     useEffect(()=>{
-        let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=d2ae75dc41e34ebfa4bde1b6d7895927`;
+        let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=d2ae75dc41e34ebfa4bde1b6d7895927=${import.meta.env.API_KEY}`;
     })
   return (
     <div>
