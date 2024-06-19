d5f951189b91272e06ad0257ce1aed83b937374f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-19 22:39:41 +0530

    added the url

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const Newsboard = () => {
-    const [articles,setArticles]=useState()
+    const [articles,setArticles]=useState([]);
+    useEffect(()=>{
+        let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=d2ae75dc41e34ebfa4bde1b6d7895927`;
+    })
   return (
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
