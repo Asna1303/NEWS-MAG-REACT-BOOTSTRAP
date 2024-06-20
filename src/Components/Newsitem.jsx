48f91d15bd32381a88f643a8858859b2937395bb
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-06-20 15:38:18 +0530

    modified the bootstrap code for card

--- src/Components/Newsitem.jsx
+++ src/Components/Newsitem.jsx
@@ -1,14 +1,14 @@
 
 
 
-const Newsitem = () => {
+const Newsitem = ({title,description,src,url}) => {
   return (
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
+  <img src={src} class="card-img-top" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">{title}</h5>
+    <p class="card-text">{description}</p>
+    <a href={url} class="btn btn-primary">Read More</a>
   </div>
 </div>
   )
